687978abe01a87a74760d1db6dd01591a947bc2d
sLurPPPeRsTaR <monsherr@hotmail.com>
2022-04-25 20:22:41 +0700

    implement checkbox

--- src/screen/Home/index.js
+++ src/screen/Home/index.js
@@ -1,10 +1,15 @@
 import {View, Text, TextInput, Button, FlatList} from 'react-native';
 import React, {useState, useEffect} from 'react';
 import Axios from 'axios';
+import CheckBox from '@react-native-community/checkbox';
+
 const base_URL = 'http://94.74.86.174:8080/api';
 
 export const HomePage = ({route}) => {
   const [activities, setActivities] = useState([]);
+  const [activity, setActivity] = useState({});
+  const [toggleCheckBox, setToggleCheckBox] = useState(false);
+
   const tokenAPI = route.params;
   const config = {
     headers: {Authorization: `Bearer ${tokenAPI}`},
@@ -16,22 +21,86 @@ export const HomePage = ({route}) => {
   const getAPI = () => {
     Axios.get(`${base_URL}/checklist`, config)
       .then(res => {
-        console.log(res.data.data);
+        // console.log(res.data.data);
         setActivities(res.data.data);
       })
+      .catch(() => alert('No Internet Connection/Internet Connection Error!'));
+  };
+
+  const onChangeHandler = (key, value) => {
+    setActivity({
+      ...activity,
+      [key]: value,
+    });
+  };
+
+  const submitHandler = () => {
+    Axios.post(`${base_URL}/checklist`, activity, config)
+      .then(res => {
+        console.log('success');
+        setActivities([...activities, res]);
+        getAPI();
+        setActivity('');
+      })
+      .catch(err => console.log(err));
+  };
+  console.log(activities);
+
+  const delHandler = item => {
+    // console.log(item.id);
+    // const copy = [...activities];
+    // const filter = copy.filter(el => {
+    //   return el.id != item.id;
+    // });
+    // Axios.post(`${base_URL}/checklist`, filter, config)
+    //   .then(() => {
+    //     console.log('success');
+    //     getAPI();
+    //   })
+    //   .catch(err => console.log(err));
+
+    Axios.delete(`${base_URL}/checklist/${item.id}`)
+      .then(() => console.log('success'))
       .catch(err => console.log(err));
   };
 
   return (
     <View>
       <Text>Checklist</Text>
+      <Text>your daily : </Text>
+      <TextInput
+        style={{borderWidth: 1}}
+        onChangeText={value => {
+          onChangeHandler('name', value);
+        }}
+      />
+      <TextInput
+        style={{borderWidth: 1}}
+        onChangeText={value => {
+          onChangeHandler('items', value);
+        }}
+      />
+      <Button title="submit" onPress={submitHandler} />
+
       <FlatList
         data={activities}
-        key={items => items.id}
-        render={({items}) => {
+        key={item => item.id}
+        renderItem={({item}) => {
           return (
-            <View>
-              <Text>{items.daily}</Text>
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                marginTop: 15,
+              }}>
+              <CheckBox
+                disabled={false}
+                value={toggleCheckBox}
+                onValueChange={newValue => setToggleCheckBox(newValue)}
+              />
+              <Text style={{flex: 1}}>{item.name}</Text>
+              <Text>{item.items}</Text>
+              <Button title="del" onPress={() => delHandler(item)} />
             </View>
           );
         }}
